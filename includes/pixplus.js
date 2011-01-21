45e1d1d2fae23759e16189ee8f4814244458e036
wowo <crckyl@myopera.com>
2011-01-21 23:20:08 +0000

    fix for firefox

--- includes/pixplus.js
+++ includes/pixplus.js
@@ -730,7 +730,7 @@
        }, false);
      export_form.textContent = 'Export/Import:';
      export_form.appendChild(export_input);
-     if ((options_page || window.opera) && window.JSON && st.u) {
+     if ((options_page || window.opera || !_extension_data) && window.JSON && st.u) {
        var btn_import = window.document.createElement('input');
        btn_import.type = 'submit';
        btn_import.value = 'Import';
@@ -814,7 +814,7 @@
            cell.appendChild(label);
            cell.setAttribute('colspan', '2');
          } else {
-           cell.innerText = key;
+           cell.textContent = key;
            input.value = value;
            cell = row.insertCell(-1);
            cell.className = 'pp-conf-cell-value';
@@ -823,7 +823,7 @@
          input_table[sec.name + '_' + key] = input;
 
          var def = window.document.createElement('button');
-         def.innerText = 'Default';
+         def.textContent = 'Default';
          def.addEventListener(
            'click',
            function() {
@@ -836,7 +836,7 @@
              update_export();
            }, false);
          row.insertCell(-1).appendChild(def);
-         row.insertCell(-1).innerText = sec.schema[key][1];
+         row.insertCell(-1).textContent = sec.schema[key][1];
 
          input.addEventListener(
            sec.schema[key][2] || type == 'boolean' ? 'change' : 'keyup',
@@ -859,7 +859,7 @@
        });
 
      var tocont = make_custom_section('Tag order');
-     tocont.innerText = '\u30d6\u30c3\u30af\u30de\u30fc\u30af\u30bf\u30b0\u306e\u4e26\u3079\u66ff' +
+     tocont.textContent = '\u30d6\u30c3\u30af\u30de\u30fc\u30af\u30bf\u30b0\u306e\u4e26\u3079\u66ff' +
        '\u3048\u3068\u30b0\u30eb\u30fc\u30d4\u30f3\u30b0\u30021\u884c1\u30bf\u30b0\u3002\n' +
        '"-": \u30bb\u30d1\u30ec\u30fc\u30bf\n"*": \u6b8b\u308a\u5168\u90e8';
      var tag_order_textarea = window.document.createElement('textarea');
@@ -877,14 +877,14 @@
      tocont.appendChild(tag_order_textarea);
 
      var tacont = make_custom_section('Tag alias');
-     tacont.innerText = '\u30b9\u30da\u30fc\u30b9\u533a\u5207\u308a\u3067\u8907\u6570\u8a18\u8ff0\u3002\u30d6\u30c3' +
+     tacont.textContent = '\u30b9\u30da\u30fc\u30b9\u533a\u5207\u308a\u3067\u8907\u6570\u8a18\u8ff0\u3002\u30d6\u30c3' +
        '\u30af\u30de\u30fc\u30af\u6642\u306e\u30bf\u30b0\u306e\u81ea\u52d5\u5165\u529b\u306b\u4f7f\u7528\u3002';
      var tag_alias_table = window.document.createElement('table');
      tag_alias_table.id = 'pp-conf-bookmark-tag_aliases';
      tacont.appendChild(tag_alias_table);
      (function() {
         var add = window.document.createElement('button');
-        add.innerText = 'Add';
+        add.textContent = 'Add';
         add.addEventListener('click', function() { add_row(); }, false);
         tacont.appendChild(add);
 
@@ -894,7 +894,7 @@
         function add_row(tag, list) {
           var row = tag_alias_table.insertRow(-1), cell, remove, input1, input2;
           remove = window.document.createElement('button');
-          remove.innerText = 'Remove';
+          remove.textContent = 'Remove';
           remove.addEventListener(
             'click',
             function() {
@@ -939,7 +939,7 @@
        row.className = 'pp-conf-section';
        var cell = row.insertCell(-1);
        cell.setAttribute('colspan', '4');
-       cell.innerText = label;
+       cell.textContent = label;
        idx = 0;
        return row;
      }
@@ -1788,11 +1788,14 @@
                '.works_caption hr, #pp-popup hr{display:block;border:none;height:1px;background-color:silver;}' +
                'hr + br, hr ~ br{display:none;}' +
                // 設定
-               '.pp-conf-section{display:block;color:#333333;text-decoration:none;font-weight:bold;margin-top:1em;}' +
+               '#pp-conf-head form{display:inline;}' +
+               '#pp-conf-head input{margin-left:0.2em;}' +
+               '#pp-conf-head a{margin-left:1em;}' +
+       '.pp-conf-section{display:block;color:#333333;text-decoration:none;font-weight:bold;margin-top:1em;}' +
                '.pp-conf-content{margin-left:1em;}' +
                '#pp-conf-root button{display:block !important;}' +
                '#pp-conf-root textarea{width:100%;}' +
-               '.pp-conf-cell-value select, .pp-conf-cell-value input{margin:0px;width:100%;box-sizing:border-box;}' +
+               '.pp-conf-cell-value select, .pp-conf-cell-value input{margin:0px;width:100%;padding:0px;}' +
                '#pp-conf-bookmark-tag_aliases .pp-conf-cell-aliases{width:100%}' +
                '#pp-conf-bookmark-tag_aliases .pp-conf-cell-aliases input{width:100%}' +
                // ポップアップ/検索欄がz-index:1000なので
