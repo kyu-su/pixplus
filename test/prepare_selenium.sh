37081c4c6f83d466c31939b26bef5eed4f1db6f7
wowo <crckyl@myopera.com>
2013-07-06 22:06:08 +0900

    [Test] Improve selenium build script

--- test/prepare_selenium.sh
+++ test/prepare_selenium.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-git clone https://code.google.com/p/selenium/
-cd selenium
+if test ! -d selenium; then
+  git clone https://code.google.com/p/selenium/
+  cd selenium
+else
+  cd selenium
+  git reset --hard
+  git pull
+  rm -rf py/selenium/webdriver/firefox/{x86,amd64,webdriver.xpi}
+  rm selenium-standalone.jar
+fi
+
 sed -i.bak 's/ -Xmx2048m / -Xmx1024m /' go
-./go //java/server/src/org/openqa/selenium/remote/server:server:uber //java/client/src/org/openqa/selenium:client-combined:project
+./go clean
+./go selenium-server-standalone
 cp build/javascript/firefox-driver/webdriver.xpi py/selenium/webdriver/firefox/
 cp -r build/java/client/src/org/openqa/selenium/firefox/{x86,amd64} py/selenium/webdriver/firefox/
-cp build/java/server/src/org/openqa/selenium/remote/server/server-standalone.jar ./
+cp build/java/server/src/org/openqa/grid/selenium/selenium-standalone.jar ./
