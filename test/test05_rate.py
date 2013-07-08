3e57caf8ccf8aa7d1e56903d0fabf9a53b0277eb
wowo <crckyl@myopera.com>
2013-07-08 22:01:22 +0900

    [Test] Add conf.general.rate_confirm=true check

--- test/test05_rate.py
+++ test/test05_rate.py
@@ -8,29 +8,26 @@ from test_base import TestCase
 class Test_Rate(TestCase):
   repeatable = False
 
-  def test_rate(self):
-    self.open_test_user()
-    if not self.browser.supports_alert:
-      self.set_conf('general.rate_confirm', False)
-      pass
-
+  def check(self, confirm):
     rating = self.find_illust(lambda popup: self.qa('#pp-popup-rating .score .rating:not(.rated)', popup))
     rating = rating[0]
     self.js('pixplus.popup.show_caption()')
     self.assertEquals(self.popup_get_illust_data('rated'), False)
 
-    if self.browser.name == 'safari':
-      warnings.warn('ActionChains.move_to_element_with_offset() is currently not supported by safari driver', FutureWarning)
-      rating.click()
-    else:
-      ac = ActionChains(self.driver)
-      ac.move_to_element_with_offset(rating, 210, 10).perform()
-      # click() moves cursor to center on Firefox22
-      ac.click_and_hold().release().perform()
+    score = (int(time.time()) % 9) + 1
+    width = score * 26
+
+    ac = ActionChains(self.driver)
+    ac.move_to_element_with_offset(rating, width - 10, 10).perform()
+    # click() moves cursor to center on Firefox22
+    ac.click_and_hold().release().perform()
+
+    if confirm:
       self.alert_accept()
-      rate = self.q('.rate', rating)
-      self.assertEqual(rate.size['width'], 234)
       pass
+
+    rate = self.q('.rate', rating)
+    self.assertEqual(rate.size['width'], width)
 
     self.assertTrue(self.qa('#pp-popup-rating .score .rating.rated'))
 
@@ -38,6 +35,22 @@ class Test_Rate(TestCase):
 
     self.assertTrue(self.qa('#pp-popup-rating .score .rating.rated'))
     self.assertEquals(self.popup_get_illust_data('rated'), True)
+    pass
+
+  def test_rate(self):
+    if not self.browser.supports_alert:
+      warnings.warn('%s is not supports alert handling' % self.browser.name)
+      return
+
+    self.open_test_user()
+    self.set_conf('general.rate_confirm', True)
+    self.check(True)
+    pass
+
+  def test_rate_noconfirm(self):
+    self.open_test_user()
+    self.set_conf('general.rate_confirm', False)
+    self.check(False)
     pass
 
   pass
