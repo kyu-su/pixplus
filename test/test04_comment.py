3bf6ff98556816ef829bd511e5b6909b1ee8097a
wowo <crckyl@myopera.com>
2013-07-18 00:47:21 +0900

    [Test] Add test for conf.popup.show_comment_form

--- test/test04_comment.py
+++ test/test04_comment.py
@@ -32,4 +32,37 @@ class Test_Comment(TestCase):
     self.assertTrue(self.xa(xpath))
     pass
 
+  def check_toggle_comment_form(self, visible):
+    popup = self.open_popup()
+
+    self.assertEquals(self.get_conf('popup.show_comment_form'), visible)
+
+    self.js('pixplus.popup.comment.start()')
+    self.popup_wait_load()
+
+    comment = self.q('#pp-popup-comment')
+    form = self.q('#pp-popup-comment-form')
+
+    self.assertTrue(comment.is_displayed())
+    self.assertEquals(form.is_displayed(), visible)
+
+    ac = self.ac()
+    ac.move_to_element_with_offset(comment, 1, 30).perform()
+    ac.click_and_hold().release().perform()
+
+    self.assertTrue(comment.is_displayed())
+    self.assertEquals(form.is_displayed(), not visible)
+    pass
+
+  def test_comment2(self):
+    self.open_test_user()
+    self.check_toggle_comment_form(True)
+    time.sleep(1)
+    self.reload()
+    self.check_toggle_comment_form(False)
+    time.sleep(1)
+    self.reload()
+    self.check_toggle_comment_form(True)
+    pass
+
   pass
