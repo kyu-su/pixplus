870f50b9e6db33fd4150581925f03a358f3ae63c
wowo <crckyl@gmail.com>
2014-02-21 22:19:59 +0900

    Fix safari/prepare_sign.sh output directory

--- safari/prepare_sign.sh
+++ safari/prepare_sign.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ROOT=`dirname $0`/..
-OUT=`dirname $0`/pixplus.safariextension/sign
+OUT=`dirname $0`/sign
 mkdir -p $OUT
+echo "out: $OUT"
 $ROOT/ext/xar/xar/src/xar --extract-certs $OUT -f $1
 openssl pkcs12 -in $2 -nodes -nocerts -out $OUT/key.pem
