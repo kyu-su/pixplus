7b6d666a3e7b8f2e110eb8080b9a1e54084a4994
wowo <crckyl@gmail.com>
2014-02-28 22:59:12 +0900

    Improve key handling

--- pixplus.js
+++ pixplus.js
@@ -163,6 +163,8 @@
         events = [events];
       }
 
+      // _.debug('listen: ' + targets.join(',') + ' ' + events.join(',') + ' capture:' + !!options.capture);
+
       var wrapper = function(ev) {
         if (options.async) {
           if (throttling_timer) {
@@ -661,10 +663,15 @@
   };
 
   _.key = {
-    code_map: { },
-    name_map: { },
-    encode_map: {Spacebar: 'Space', Esc: 'Escape'},
-    decode_map: { },
+    keycode_map: { },
+    canonical_map: {
+      Spacebar: 'Space',
+      Esc: 'Escape',
+      '+': 'plus',
+      ',': 'comma',
+      ' ': 'Space',
+      '\t': 'Tab'
+    },
     global: {
       connection: null,
       handlers: []
@@ -674,12 +681,13 @@
       var keys = [], key, chr = ev['char'];
 
       var k = ev.keyCode, c = ev.charCode;
-      if (c >= 0x20 && c < 0x7f) {
+      if (ev.type === 'keypress' && c >= 0x20 && c < 0x7f) {
         key = g.String.fromCharCode(c).toLowerCase();
-        keys.push(this.encode_map[key] || key);
-      } else if (this.code_map[k]) {
-        keys.push(this.code_map[k]);
+      } else {
+        key = this.keycode_map[k];
       }
+
+      keys.push(this.canonical_map[key] || key);
 
       if (keys.length < 1) {
         return null;
@@ -729,7 +737,7 @@
       if (!this.global.connection) {
         _.debug('key.listen_global: begin');
 
-        this.global.connection = this.listen(d, function() {
+        this.global.connection = this.listen(w, function() {
           for(var i = 0; i < that.global.handlers.length; ++i) {
             var ret = that.global.handlers[i].apply(this, arguments);
             if (ret) {
@@ -737,7 +745,7 @@
             }
           }
           return false;
-        });
+        }, {capture: true});
       }
 
       this.global.handlers.unshift(listener);
@@ -761,32 +769,29 @@
     init: function() {
       var that = this;
 
-      var keys = [
-        [8, 'Backspace'], [9, 'Tab'], [13, 'Enter'],
-        [16, 'Shift'], [17, 'Control'], [18, 'Alt'], [27, 'Escape'],
-        [32, 'Space'], [33, 'PageUp'], [34, 'PageDown'], [35, 'End'],
-        [36, 'Home'], [37, 'Left'], [38, 'Up'], [39, 'Right'],
-        [40, 'Down'], [45, 'Insert'], [46, 'Delete']
-      ];
-
-      for(var i = 1; i < 13; ++i) {
-        keys.push([111 + i, 'F' + i.toString(10)]);
-      }
-
-      keys.forEach(function(p) {
-        var code = p[0], name = p[1];
-        that.code_map[code] = name;
-        that.name_map[name] = code;
-      });
-
       [
-        ['+', 'plus'],
-        [',', 'comma'],
-        [' ', 'Space'],
-        ['\t', 'Tab']
+
+        [8, 'Backspace'], [9, 'Tab'], [13, 'Enter'], [16, 'Shift'], [17, 'Control'], [18, 'Alt'],
+        [27, 'Escape'], [32, 'Space'], [33, 'PageUp'], [34, 'PageDown'], [35, 'End'], [36, 'Home'],
+        [37, 'Left'], [38, 'Up'], [39, 'Right'], [40, 'Down'], [45, 'Insert'], [46, 'Delete'],
+        [48, '0'], [49, '1'], [50, '2'], [51, '3'], [52, '4'], [53, '5'], [54, '6'], [55, '7'],
+        [56, '8'], [57, '9'], [58, ':'], [59, ';'], [60, '<'], [61, '='], [62, '>'], [63, '?'],
+        [64, '@'], [65, 'a'], [66, 'b'], [67, 'c'], [68, 'd'], [69, 'e'], [70, 'f'], [71, 'g'],
+        [72, 'h'], [73, 'i'], [74, 'j'], [75, 'k'], [76, 'l'], [77, 'm'], [78, 'n'], [79, 'o'],
+        [80, 'p'], [81, 'q'], [82, 'r'], [83, 's'], [84, 't'], [85, 'u'], [86, 'v'], [87, 'w'],
+        [88, 'x'], [89, 'y'], [90, 'z'], [96, '0'], [97, '1'], [98, '2'], [99, '3'], [100, '4'],
+        [101, '5'], [102, '6'], [103, '7'], [104, '8'], [105, '9'], [106, '*'], [107, '+'],
+        [109, '-'], [110, '.'], [111, '/'], [112, 'F1'], [113, 'F2'], [114, 'F3'], [115, 'F4'],
+        [116, 'F5'], [117, 'F6'], [118, 'F7'], [119, 'F8'], [120, 'F9'], [121, 'F10'], [122, 'F11'],
+        [123, 'F12'], [124, 'F13'], [125, 'F14'], [126, 'F15'], [127, 'F16'], [128, 'F17'], [129, 'F18'],
+        [130, 'F19'], [131, 'F20'], [132, 'F21'], [133, 'F22'], [134, 'F23'], [135, 'F24'], [160, '^'],
+        [161, '!'], [162, '"'], [163, '#'], [164, '$'], [165, '%'], [166, '&'], [167, '_'], [168, '('],
+        [169, ')'], [170, '*'], [171, '+'], [172, '|'], [173, '-'], [174, '{'], [175, '}'], [176, '~'],
+        [188, ','], [190, '.'], [191, '/'], [192, '`'], [219, '['], [220, '\\'], [221, ']'], [222, "'"]
+
       ].forEach(function(p) {
-        that.encode_map[p[0]] = p[1];
-        that.decode_map[p[1]] = p[0];
+        var code = p[0], name = p[1];
+        that.keycode_map[code] = name;
       });
     }
   };
