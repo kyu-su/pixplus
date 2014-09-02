ee8abfcf33f9263f87bf1e2efb81ea6f8f6a3d91
wowo <crckyl@gmail.com>
2014-09-02 22:34:04 +0900

    Bump version

--- bin/pixplus.user.js
+++ bin/pixplus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        pixplus.js
 // @author      wowo
-// @version     1.11.1
+// @version     1.12.0
 // @license     The MIT License
 // @description hogehoge
 // @icon        http://ccl4.info/pixplus/pixplus_48.png
@@ -6929,7 +6929,7 @@ input[type="text"]:focus~#pp-search-ratio-custom-preview{display:block}\
   _.changelog = [
     {
       "date": "2014/xx/xx",
-      "version": "1.11.1",
+      "version": "1.12.0",
       "releasenote": "",
       "changes_i18n": {
         "en": [
