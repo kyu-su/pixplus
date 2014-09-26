ffc4911862af371ce8ec85992b1a341435d4241b
wowo <crckyl@gmail.com>
2014-09-26 22:49:20 +0900

    Fix autoupdate file

--- autoupdate/1/pixplus.user.js
+++ autoupdate/1/pixplus.user.js
@@ -7,10 +7,10 @@
 // @icon        http://ccl4.info/pixplus/pixplus_48.png
 // @icon64      http://ccl4.info/pixplus/pixplus_64.png
 // @namespace   http://my.opera.com/crckyl/
-// @updateURL   https://raw.githubusercontent.com/crckyl/pixplus/master/autoupdate/1/metadata.user.js
 // @include     http://www.pixiv.net/*
 // @exclude     *pixivreader*
 // @run-at      document-end
+// @downloadURL https://raw.githubusercontent.com/crckyl/pixplus/master/autoupdate/1/metadata.user.js
 // ==/UserScript==
 
 (function(entrypoint) {
