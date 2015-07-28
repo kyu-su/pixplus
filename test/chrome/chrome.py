5d6830cbd806dd1afec440408dcffa087ecfd67e
wowo <crckyl@gmail.com>
2015-07-28 22:50:26 +0900

    [Test] Added --chrome-command option

--- test/chrome/chrome.py
+++ test/chrome/chrome.py
@@ -15,10 +15,17 @@ class Chrome(Browser):
         self.read_file_as_base64(self.download_crx('ab.crx', 'gighmmpiobklfepjocnamgkkbiglidom'))
       ]
     }
+    if self.args.chrome_bin:
+      caps['chromeOptions']['binary'] = self.args.chrome_bin
+      pass
     pass
 
   def download_crx(self, filename, ext_id, chrome_version = '37.0.2062.120'):
     url = 'https://clients2.google.com/service/update2/crx?response=redirect&prodversion=%s&x=id%%3D%s%%26uc' % (chrome_version, ext_id)
     return self.download(url, filename)
 
+  pass
+
+def register_args(parser):
+  parser.add_argument('--chrome-command', dest = 'chrome_bin')
   pass
