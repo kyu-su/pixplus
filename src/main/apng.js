f2c0b245c6138a4026f72886e3dd868534eacb04
wowo <crckyl@gmail.com>
2015-08-10 18:43:44 +0900

    Split apng.{b64,crc} modules into lib/{base64,crc32}.js files

--- src/main/apng.js
+++ src/main/apng.js
@@ -1,5 +1,7 @@
 (function(generator) {
   _.apng = generator();
+  _.apng.base64 = _.base64;
+  _.apng.crc32 = _.crc32;
   _.apng.__mod_generator = generator;
 
   var APNGDialog = _.class.create(_.Dialog.prototype, {
@@ -124,153 +126,6 @@
 
 })(function() {
   return {
-    crc: {
-      table: [
-        0, 1996959894, 3993919788, 2567524794, 124634137, 1886057615, 3915621685, 2657392035,
-        249268274, 2044508324, 3772115230, 2547177864, 162941995, 2125561021, 3887607047, 2428444049,
-        498536548, 1789927666, 4089016648, 2227061214, 450548861, 1843258603, 4107580753, 2211677639,
-        325883990, 1684777152, 4251122042, 2321926636, 335633487, 1661365465, 4195302755, 2366115317,
-        997073096, 1281953886, 3579855332, 2724688242, 1006888145, 1258607687, 3524101629, 2768942443,
-        901097722, 1119000684, 3686517206, 2898065728, 853044451, 1172266101, 3705015759, 2882616665,
-        651767980, 1373503546, 3369554304, 3218104598, 565507253, 1454621731, 3485111705, 3099436303,
-        671266974, 1594198024, 3322730930, 2970347812, 795835527, 1483230225, 3244367275, 3060149565,
-        1994146192, 31158534, 2563907772, 4023717930, 1907459465, 112637215, 2680153253, 3904427059,
-        2013776290, 251722036, 2517215374, 3775830040, 2137656763, 141376813, 2439277719, 3865271297,
-        1802195444, 476864866, 2238001368, 4066508878, 1812370925, 453092731, 2181625025, 4111451223,
-        1706088902, 314042704, 2344532202, 4240017532, 1658658271, 366619977, 2362670323, 4224994405,
-        1303535960, 984961486, 2747007092, 3569037538, 1256170817, 1037604311, 2765210733, 3554079995,
-        1131014506, 879679996, 2909243462, 3663771856, 1141124467, 855842277, 2852801631, 3708648649,
-        1342533948, 654459306, 3188396048, 3373015174, 1466479909, 544179635, 3110523913, 3462522015,
-        1591671054, 702138776, 2966460450, 3352799412, 1504918807, 783551873, 3082640443, 3233442989,
-        3988292384, 2596254646, 62317068, 1957810842, 3939845945, 2647816111, 81470997, 1943803523,
-        3814918930, 2489596804, 225274430, 2053790376, 3826175755, 2466906013, 167816743, 2097651377,
-        4027552580, 2265490386, 503444072, 1762050814, 4150417245, 2154129355, 426522225, 1852507879,
-        4275313526, 2312317920, 282753626, 1742555852, 4189708143, 2394877945, 397917763, 1622183637,
-        3604390888, 2714866558, 953729732, 1340076626, 3518719985, 2797360999, 1068828381, 1219638859,
-        3624741850, 2936675148, 906185462, 1090812512, 3747672003, 2825379669, 829329135, 1181335161,
-        3412177804, 3160834842, 628085408, 1382605366, 3423369109, 3138078467, 570562233, 1426400815,
-        3317316542, 2998733608, 733239954, 1555261956, 3268935591, 3050360625, 752459403, 1541320221,
-        2607071920, 3965973030, 1969922972, 40735498, 2617837225, 3943577151, 1913087877, 83908371,
-        2512341634, 3803740692, 2075208622, 213261112, 2463272603, 3855990285, 2094854071, 198958881,
-        2262029012, 4057260610, 1759359992, 534414190, 2176718541, 4139329115, 1873836001, 414664567,
-        2282248934, 4279200368, 1711684554, 285281116, 2405801727, 4167216745, 1634467795, 376229701,
-        2685067896, 3608007406, 1308918612, 956543938, 2808555105, 3495958263, 1231636301, 1047427035,
-        2932959818, 3654703836, 1088359270, 936918000, 2847714899, 3736837829, 1202900863, 817233897,
-        3183342108, 3401237130, 1404277552, 615818150, 3134207493, 3453421203, 1423857449, 601450431,
-        3009837614, 3294710456, 1567103746, 711928724, 3020668471, 3272380065, 1510334235, 755167117
-      ],
-
-      calc: function(bytesary) {
-        var crc = 0xffffffff;
-        for(var i = 0; i < bytesary.length; ++i) {
-          var data = bytesary[i];
-          for(var j = 0; j < data.length; ++j) {
-            crc = (this.table[((crc ^ data[j]) >>> 0) & 0xff] ^ (crc >>> 8)) >>> 0;
-          }
-        }
-        crc = (crc ^ 0xffffffff) >>> 0;
-        return crc;
-      }
-    },
-
-    b64: {
-      make_table: function() {
-        if (this.table) {
-          return;
-        }
-
-        var that = this;
-        this.table_dec = {};
-        this.table_enc = [];
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'.split('').forEach(function(c, i) {
-          that.table_dec[c] = i;
-          that.table_enc.push(c);
-        });
-      },
-
-      decode: function(b64str) {
-        this.make_table();
-
-        b64str = b64str.replace(/=+$/, '');
-
-        var length = Math.floor(b64str.length * 3 / 4),
-            remains = b64str.length % 4;
-        if (remains === 1) {
-          throw 'Invalid base64 length';
-        } else if (remains === 2) {
-          ++length;
-        } else if (remains === 3) {
-          length += 2;
-        }
-
-        var buf = new ArrayBuffer(length),
-            data = new Uint8Array(buf);
-
-        for(var idx = 0, wi = 0; idx < b64str.length; ++idx) {
-          var c = this.table_dec[b64str[idx]];
-          if (typeof(c) === 'undefined') {
-            throw 'Invalid character for base64 - ' + b64str[idx];
-          }
-          if ((idx % 4) === 0) {
-            data[wi++] = (c << 2) & 0xfc;
-          } else if ((idx % 4) === 1) {
-            data[wi - 1] |= (c >> 4) & 0x3;
-            data[wi++] = (c << 4) & 0xf0;
-          } else if ((idx % 4) === 2) {
-            data[wi - 1] |= (c >> 2) & 0xf;
-            data[wi++] = (c << 6) & 0xc0;
-          } else if ((idx % 4) === 3) {
-            data[wi - 1] |= c & 0x3f;
-          }
-        }
-
-        return data;
-      },
-
-      encode: function(bytesary) {
-        this.make_table();
-
-        var b64 = [];
-
-        var that = this;
-        var add = function(sixbits) {
-          var c = that.table_enc[sixbits];
-          if (!c) {
-            throw 'Base64 encode table has no entry for - 0x' + sixbits.toString(16);
-          }
-          b64.push(c);
-        };
-
-        var idx = 0, p = -1;
-        for(var i = 0; i < bytesary.length; ++i) {
-          var bytes = bytesary[i];
-          for(var j = 0; j < bytes.length; ++j) {
-            if (idx % 3 === 0) {
-              add((bytes[j] >> 2) & 0x3f);
-              p = (bytes[j] << 4) & 0x30;
-            } else if (idx % 3 === 1) {
-              add(p | ((bytes[j] >> 4) & 0xf));
-              p = (bytes[j] << 2) & 0x3c;
-            } else if (idx % 3 === 2) {
-              add(p | ((bytes[j] >> 6) & 0x3));
-              add(bytes[j] & 0x3f);
-            }
-            ++idx;
-          }
-        }
-
-        if (p >= 0) {
-          add(p);
-        }
-
-        while((b64.length % 4) !== 0) {
-          b64.push('=');
-        }
-
-        return b64.join('');
-      }
-    },
-
     onmessage: function(ev) {
       try {
         var data = ev.data;
@@ -284,7 +139,7 @@
 
         var result = this.generate_bytes(frames);
         if (data.return_b64) {
-          result = this.b64.encode(result);
+          result = this.base64.enc(result);
         }
         self.postMessage({
           command: 'complete',
@@ -323,6 +178,8 @@
       this.prepare_frames(frames, function(frames, transferables) {
         var code = [
           'apng=(', that.__mod_generator.toString(), ')();',
+          'apng.base64=(', _.base64.__mod_generator.toString(), ')();',
+          'apng.crc32=(', _.crc32.__mod_generator.toString(), ')();',
           'onmessage=apng.onmessage.bind(apng);'
         ];
 
@@ -500,7 +357,7 @@
         }, 0)),
         new Uint8Array(type)
       ].concat(bodies).concat([
-        this.uint32(this.crc.calc([type].concat(bodies)))
+        this.uint32(this.crc32.calc([type].concat(bodies)))
       ]);
     },
 
@@ -524,7 +381,7 @@
       if (frame.image_buf) {
         data = new Uint8Array(frame.image_buf);
       } else {
-        data = this.b64.decode(frame.image_b64);
+        data = this.base64.dec(frame.image_b64);
       }
 
       // http://www.w3.org/TR/PNG/#5PNG-file-signature
